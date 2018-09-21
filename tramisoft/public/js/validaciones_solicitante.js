85ab7cc8eef15500d4b6bbbb4516e94244ade49f
Erick <erickjhorman@gmail.com>
2018-09-20 21:03:36 -0500

    Se crearon las validaciones

--- tramisoft/public/js/validaciones_solicitante.js
+++ tramisoft/public/js/validaciones_solicitante.js
@@ -1,4 +1,3 @@
-alert("Mi java")
 function validar(){
 	var persona, identificacion, numero, genero, nombre, apellido, celular, telefono,
 	correo, nacionalidad, departamento, ciudad, expresion   // se cran variables para guardar informacion de los inputs y la expresiones regulares 
@@ -17,20 +16,54 @@ nacionalidad = document.getElementById("Naci").value;
 departamento = document.getElementById("Departamento").value;
 ciudad = document.getElementById("idCiudad").value;
 
-
+expresion = /\w+@\w+\.+[a-z]/; //Esto represeta la extructura de un correo 
 
 if (persona === "" || identificacion === "" || numero === "" || genero === "" ||
 nombre === "" || apellido === "" || celular === "" || telefono === "" || correo === "" ||
-nacionalidad === "" || departamento === "" ||  departamento === "ciudad" ) {
+nacionalidad === "" || departamento === "" ||  departamento === "ciudad"  ) {
 	alert("Todos los campos son obligatorios ");
 	return false;  // False no deja que se envie el formulario
 }
+
+else if (nombre.length>30){
+  alert("El nombre es muy largo ");
+	 return false;  // False no deja que se envie el formulario
+}
+
+else if (apellido.length>30){
+  alert("El apellido es muy largo ");
+	   return false;  // False no deja que se envie el formulario
+}
+
+else if (correo.length>25){
+  alert("El correo es muy largo ");
+	   return false;  // False no deja que se envie el formulario
+}
+
+else if (!expresion.test(correo)){
+ alert("El correo no es valido ");
+	  return false;  // False no deja que se envie el formulario
+ }
+
+
+else if (telefono.length>13){
+  alert("El telefono es muy largo ");
+	return false;  // False no deja que se envie el formulario
+}
+
+// Nan siginica no es numero 
+else if (isNaN(celular) && isNaN(telefono) && isNaN(numero)){  
+  alert("Los campos celular, telefono y numero de identificacion" );
+	return false;  // False no deja que se envie el formulario
+}
+
+
+
 
 // if (nombre === "") {
 // 	alert("Todos los campos son obligatorios ");
 // 	return false;  // False no deja que se envie el formulario
 // }
-
 }
 
 // //Validad con el formulario
--- tramisoft/resources/views/Solicitante/create.blade.php
+++ tramisoft/resources/views/Solicitante/create.blade.php
@@ -5,14 +5,12 @@
 @section('content')  <!-- Section y endsection son directivas para poner info del body de la pagina-->
 
 
- <form name="form1" method="POST" action="/solicitante>
- 
-
- 	@csrf <!-- Directriz para proteger los formularios de falsificacion de registros-->
+ <form name="form1" method="POST" action="/solicitante">
+  @csrf 
   <div class="form-row">
      <div class="form-group col-md-6">
      <label for="tipoPersona">Tipo persona</label>
-      <select class="form-control" name="tipoPersona" id="persona" >
+      <select class="form-control" name="tipoPersona" id="persona" required>
         <option value="o" disabled="true" selected="true">Selecione un tipo de persona</option>        
                @foreach($tipopersona as $per)
                 <option value="{{$per->id}}">{{$per->nombre}}</option>
@@ -24,7 +22,7 @@
  
     <div class="form-group col-md-6">
       <label for="identificacion">Tipo de dentificacion</label>
-     <select class="form-control" name="identificacion" id="identificacion">
+     <select class="form-control" name="identificacion" id="identificacion" required>
       <option value="o" disabled="true" selected="true">Selecione un tipo de identificacion</option>  
                 @foreach($identificacion as $id)
                 <option  value="{{$id->id}}">{{$id->tipoIdentificacion}}</option>
@@ -36,13 +34,13 @@
 
       <div class="form-group col-md-6">
       <label for="numero">Numero</label>
-        <input type="text" class="form-control" id="numero" placeholder="Ingrese el numero" name="numero" >
+        <input type="text" class="form-control" id="numero" placeholder="Ingrese el numero" name="numero"  required="">
       </div>
    
 
      <div class="form-group col-md-6">
      <label for="genero">Genero</label>
-       <select class="form-control" name="genero"  id="genero">
+       <select class="form-control" name="genero"  id="genero" required>
         <option value="o" disabled="true" selected="true">Selecione un tipo de genero</option>  
                 @foreach($genero as $gen)
                 
@@ -54,7 +52,7 @@
     
    <div class="form-group col-md-6 ">
     <label for="nombre">Nombre</label>
-   <input type="text" class="form-control" id="nombre" placeholder="nombre" name="nombre" >
+   <input type="text" class="form-control" id="nombre" placeholder="nombre" name="nombre" required>
   </div>
 
    
@@ -63,12 +61,12 @@
  
   <div class="form-group col-md-6">
     <label for="apellido">Apellido</label>
-   <input type="text" class="form-control" id="apellido" placeholder="apellido" name="apellido" >
+   <input type="text" class="form-control" id="apellido" placeholder="apellido" name="apellido"  required>
   </div>
   
   <div class="form-group col-md-6">
     <label for="celular">Celular</label>
-   <input type="text" class="form-control" id="celular" placeholder="Nº de celular" name="celular" >
+   <input type="text" class="form-control" id="celular" placeholder="Nº de celular" name="celular" required>
   </div>
 
 
@@ -81,13 +79,13 @@
 
    <div class="form-group col-md-6">
     <label for="correo">Correo</label>
-    <input type="email" class="form-control" name="correo" id="Correo" placeholder="Correo">
+    <input type="email" class="form-control" name="correo" id="Correo" placeholder="Correo" required>
   </div>
  
 
    <div class="form-group col-md-6">
      <label for="nacionalidad">Nacionalidad</label>
-        <select class="form-control" name="nacionalidades" id="Naci" >
+        <select class="form-control" name="nacionalidades" id="Naci" required="" >
               <option value="o" disabled="true" selected="true">Selecione una pais</option> 
                 @foreach($Nacionalidades as $na)
                 <option  value="{{$na->id}}">{{$na->nombre}}</option>
@@ -102,7 +100,7 @@
      <div class="form-group col-md-6">
      <label for="nacionalidad">Departamento</label>
      
-            <select class="form-control" name="departamento" id="Departamento" >
+            <select class="form-control" name="departamento" id="Departamento" required>
               <option value="o" disabled="true" selected="true">Selecione un departamento</option>  
                 
             </select>
@@ -112,7 +110,7 @@
 
     <div class="form-group col-md-6">
      <label for="ciudad">Ciudad</label>
-     <select class="form-control" name="ciudad" id="idCiudad"  >
+     <select class="form-control" name="ciudad" id="idCiudad" required>
               <option value="o" disabled="true" selected="true">Selecione una ciudad</option>  
       </select>
     </div>
@@ -176,9 +174,9 @@
                 @endforeach
      </select>
     </div> --}}
-  </div>
-   <button type="button" onclick="validar()"class="btn btn-primary">Enviar</button>
-    </form>
+    </div>
+   <button type="submit" onclick=" return validar()" class="btn btn-primary">Enviar</button>
+  
   
 </form>  
 @endsection
