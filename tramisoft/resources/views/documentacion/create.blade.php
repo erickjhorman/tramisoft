1eabd790ee546bb100b32109806002e9fc775dee
Erick <erickjhorman@gmail.com>
2018-10-02 23:02:27 -0500

    Se creo vista de documentacion

---
+++ tramisoft/resources/views/documentacion/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.documentacion')
+
+@section('title' , 'Registro documentacion')
+@section('content')  <!-- Section y endsection son directivas para poner info del body de la pagina-->
+
+
+
+@endsection
